d59376c21703d8d515e40fdb759a5c4785b623ff
GeunYeong <jgy091411@gmail.com>
2026-03-05 12:57:58 +0900

    :sparkles: feat : Nickname VO 예외 메시지 추가

--- src/main/java/io/mallang/member/domain/Nickname.java
+++ src/main/java/io/mallang/member/domain/Nickname.java
@@ -8,9 +8,13 @@ public record Nickname(String value) {
             Pattern.compile("^[가-힣a-zA-Z0-9_.\\-]([가-힣a-zA-Z0-9_.\\- ]*[가-힣a-zA-Z0-9_.\\-])?$");
 
     public Nickname {
-        if (value == null || value.isEmpty()) throw new IllegalArgumentException();
-        if (value.startsWith(" ") ||  value.endsWith(" ")) throw new IllegalArgumentException();
-        if (value.length() < 2 || value.length() > 20) throw new IllegalArgumentException();
-        if (!NICKNAME_PATTERN.matcher(value).matches()) throw new IllegalArgumentException();
+        if (value == null || value.isEmpty())
+            throw new IllegalArgumentException("닉네임은 비어 있을 수 없습니다.");
+        if (value.startsWith(" ") ||  value.endsWith(" "))
+            throw new IllegalArgumentException("닉네임은 공백으로 시작하거나 끝날 수 없습니다.");
+        if (value.length() < 2 || value.length() > 20)
+            throw new IllegalArgumentException("닉네임은 2자 이상 20자 이하여야 합니다.");
+        if (!NICKNAME_PATTERN.matcher(value).matches())
+            throw new IllegalArgumentException("닉네임은 한글, 영문자, 숫자, 밑줄(_), 점(.), 하이픈(-)만 사용할 수 있습니다.");
     }
 }
