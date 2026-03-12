a4291fb29df148d2078053ae510124276c94530a
GeunYeong <jgy091411@gmail.com>
2026-03-12 15:13:27 +0900

    :sparkles: feat : Order 애그리거트 ShippingInfo VO 정의

---
+++ src/main/java/io/mallang/order/domain/ShippingInfo.java
@@ -0,0 +1,14 @@
+package io.mallang.order.domain;
+
+import io.mallang.member.domain.Address;
+import io.mallang.member.domain.Receiver;
+
+public record ShippingInfo(Receiver receiver, Address address) {
+
+    public ShippingInfo {
+        if (receiver == null)
+            throw new IllegalArgumentException("수령인 정보는 필수입니다.");
+        if (address == null)
+            throw new IllegalArgumentException("배송지 주소는 필수입니다.");
+    }
+}
