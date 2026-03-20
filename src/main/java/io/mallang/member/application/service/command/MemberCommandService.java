c3ff3ce085b2362c92eb163bb04deb692b2028ca
GeunYeong <jgy091411@gmail.com>
2026-03-20 14:46:22 +0900

    :sparkles: feat : RegisterMemberUseCase 구현

---
+++ src/main/java/io/mallang/member/application/service/command/MemberCommandService.java
@@ -0,0 +1,54 @@
+package io.mallang.member.application.service.command;
+
+import io.mallang.domain.common.ClockHolder;
+import io.mallang.domain.common.IdGenerator;
+import io.mallang.member.application.provided.command.RegisterMemberUseCase;
+import io.mallang.member.application.provided.command.model.RegisterMemberResult;
+import io.mallang.member.application.required.command.SaveMemberPort;
+import io.mallang.member.application.required.query.LoadMemberPort;
+import io.mallang.member.domain.DuplicateEmailException;
+import io.mallang.member.domain.DuplicateNicknameException;
+import io.mallang.member.domain.Email;
+import io.mallang.member.domain.Member;
+import io.mallang.member.domain.MemberCreateCommand;
+import io.mallang.member.domain.Nickname;
+import io.mallang.member.domain.PasswordEncoder;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MemberCommandService implements RegisterMemberUseCase {
+
+    private final IdGenerator idGenerator;
+    private final ClockHolder clockHolder;
+    private final SaveMemberPort saveMemberPort;
+    private final LoadMemberPort loadMemberPort;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public RegisterMemberResult register(MemberCreateCommand createCommand) {
+        Email email = new Email(createCommand.email());
+        Nickname nickname = new Nickname(createCommand.nickname());
+
+        validateDuplicateEmail(email);
+        validateDuplicateNickname(nickname);
+
+        Member member = Member.create(createCommand, passwordEncoder, idGenerator, clockHolder);
+        saveMemberPort.save(member);
+
+        return new RegisterMemberResult(member.getId().value());
+    }
+
+    private void validateDuplicateEmail(Email email) {
+        if (loadMemberPort.existsByEmail(email)) {
+            throw new DuplicateEmailException(email);
+        }
+    }
+
+    private void validateDuplicateNickname(Nickname nickname) {
+        if (loadMemberPort.existsByNickname(nickname)) {
+            throw new DuplicateNicknameException(nickname);
+        }
+    }
+}
