a6ed206c39a8655ae49e36201dcf3eb5806a2711
GeunYeong <jgy091411@gmail.com>
2026-03-23 12:00:42 +0900

    :sparkles: feat : UpdateShippingAddressUseCase 구현

--- src/main/java/io/mallang/member/application/service/command/ShippingAddressCommandService.java
+++ src/main/java/io/mallang/member/application/service/command/ShippingAddressCommandService.java
@@ -3,14 +3,17 @@ package io.mallang.member.application.service.command;
 import io.mallang.domain.common.IdGenerator;
 import io.mallang.member.application.provided.command.RegisterShippingAddressUseCase;
 import io.mallang.member.application.provided.command.UpdateDefaultShippingAddressUseCase;
+import io.mallang.member.application.provided.command.UpdateShippingAddressUseCase;
 import io.mallang.member.application.provided.command.model.RegisterShippingAddressCommand;
 import io.mallang.member.application.provided.command.model.UpdateDefaultShippingAddressCommand;
+import io.mallang.member.application.provided.command.model.UpdateShippingAddressCommand;
 import io.mallang.member.application.required.command.SaveMemberPort;
 import io.mallang.member.application.required.query.LoadMemberPort;
 import io.mallang.member.domain.Member;
 import io.mallang.member.domain.ShippingAddress;
 import io.mallang.member.domain.ShippingAddressId;
 import io.mallang.member.domain.command.AddShippingAddressCommand;
+import io.mallang.member.domain.command.ModifyShippingAddressCommand;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,7 +21,7 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @Transactional
 @RequiredArgsConstructor
-public class ShippingAddressCommandService implements RegisterShippingAddressUseCase, UpdateDefaultShippingAddressUseCase {
+public class ShippingAddressCommandService implements RegisterShippingAddressUseCase, UpdateDefaultShippingAddressUseCase, UpdateShippingAddressUseCase {
 
     private final IdGenerator idGenerator;
     private final LoadMemberPort loadMemberPort;
@@ -49,6 +52,24 @@ public class ShippingAddressCommandService implements RegisterShippingAddressUse
         Member member = loadMemberPort.getById(command.memberId());
 
         member.setDefaultShippingAddress(command.shippingAddressId());
+
+        saveMemberPort.save(member);
+    }
+
+    @Override
+    public void update(UpdateShippingAddressCommand command) {
+        Member member = loadMemberPort.getById(command.memberId());
+
+        member.modifyShippingAddress(
+                command.shippingAddressId(),
+                new ModifyShippingAddressCommand(
+                        command.receiverName(),
+                        command.receiverPhoneNumber(),
+                        command.zipCode(),
+                        command.mainAddress(),
+                        command.detailAddress()
+                )
+        );
 
         saveMemberPort.save(member);
     }
--- src/main/java/io/mallang/member/domain/Member.java
+++ src/main/java/io/mallang/member/domain/Member.java
@@ -99,8 +99,9 @@ public class Member {
     }
 
     public ShippingAddress modifyShippingAddress(ShippingAddressId id, ModifyShippingAddressCommand command) {
-        if (!status.isActive())
+        if (!status.isActive()) {
             throw new IllegalStateException("ACTIVE 상태에서만 배송지를 수정할 수 있습니다.");
+        }
 
         return shippingAddresses.modify(id, command);
     }
