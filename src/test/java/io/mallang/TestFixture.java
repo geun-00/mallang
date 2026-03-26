328e2169bad5bdef6f143733a985f99c82353956
GeunYeong <jgy091411@gmail.com>
2026-03-26 10:37:41 +0900

    :sparkles: feat : 상품 대표 이미지 변경 API 명세 및 테스트 추가

--- src/test/java/io/mallang/TestFixture.java
+++ src/test/java/io/mallang/TestFixture.java
@@ -207,6 +207,15 @@ public record TestFixture(TestRestTemplate client) {
         );
     }
 
+    public ResponseEntity<Void> changeThumbnailImage(String productId, String imageId) {
+        return client.exchange(
+                RequestEntity
+                        .patch("/products/" + productId + "/images/" + imageId + "/thumbnail")
+                        .build(),
+                Void.class
+        );
+    }
+
     public String registerProductThenGetId() {
         ResponseEntity<Void> response = registerProduct(generateCreateProductRequest());
         return response.getHeaders().getLocation().getPath().substring("/products/".length());
