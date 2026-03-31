122285f1af1eaf4ccce18fd409d4ba32407cf6b2
GeunYeong <jgy091411@gmail.com>
2026-03-31 15:30:47 +0900

    :sparkles: feat : JPA Audit 기능 추가

---
+++ src/main/java/io/mallang/adapter/persistence/jpa/BaseTimeEntity.java
@@ -0,0 +1,22 @@
+package io.mallang.adapter.persistence.jpa;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseTimeEntity {
+
+    @CreatedDate
+    @Column(updatable = false)
+    protected LocalDateTime createdDate;
+
+    @LastModifiedDate
+    protected LocalDateTime lastModifiedDate;
+}
