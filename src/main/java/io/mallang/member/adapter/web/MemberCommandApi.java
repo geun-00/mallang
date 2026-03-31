9da9bf57a764b74e0e912b60e46fe201fa298033
GeunYeong <jgy091411@gmail.com>
2026-03-31 17:26:00 +0900

    :recycle: refactor : Member 애그리거트 adapter 계층 테스트 클래스 개선

--- src/main/java/io/mallang/member/adapter/web/MemberCommandApi.java
+++ src/main/java/io/mallang/member/adapter/web/MemberCommandApi.java
@@ -10,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
 
@@ -29,6 +30,11 @@ public class MemberCommandApi {
                 )
         );
 
-        return ResponseEntity.created(URI.create("/members/" + result.memberId())).build();
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                                                  .path("/{id}")
+                                                  .buildAndExpand(result.memberId())
+                                                  .toUri();
+
+        return ResponseEntity.created(location).build();
     }
 }
