e1a6aa8c41d13a433542234ffd7735eefd196f7a
GeunYeong <jgy091411@gmail.com>
2026-04-05 15:41:45 +0900

    :recycle: refactor : AI 코드 리뷰 반영

--- src/main/java/io/mallang/P6SpyFormatter.java
+++ src/main/java/io/mallang/P6SpyFormatter.java
@@ -3,14 +3,14 @@ package io.mallang;
 import com.p6spy.engine.spy.appender.MessageFormattingStrategy;
 import org.hibernate.engine.jdbc.internal.BasicFormatterImpl;
 import org.hibernate.engine.jdbc.internal.Formatter;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ComponentScan;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayDeque;
 import java.util.Deque;
 
-@Configuration
+@ComponentScan
 public class P6SpyFormatter implements MessageFormattingStrategy {
 
     private static final String BASE_PACKAGE = "io.mallang";
