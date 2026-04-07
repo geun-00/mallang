5493996b260cf1eebf392872b01a43cd8fe516e0
GeunYeong <jgy091411@gmail.com>
2026-04-07 11:13:12 +0900

    :sparkles: feat : LoadProductDetailPort 구현

--- src/main/java/io/mallang/product/adapter/persistence/mybatis/ProductQueryMapper.java
+++ src/main/java/io/mallang/product/adapter/persistence/mybatis/ProductQueryMapper.java
@@ -1,8 +1,10 @@
 package io.mallang.product.adapter.persistence.mybatis;
 
+import io.mallang.product.adapter.persistence.mybatis.model.ProductDetailRow;
 import io.mallang.product.adapter.persistence.mybatis.model.ProductListRow;
 import io.mallang.product.adapter.persistence.mybatis.model.SearchProductCondition;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
@@ -10,4 +12,6 @@ import java.util.List;
 public interface ProductQueryMapper {
 
     List<ProductListRow> selectProducts(SearchProductCondition condition);
+
+    List<ProductDetailRow> selectProductDetailRows(@Param("productIdValue") String productIdValue);
 }
