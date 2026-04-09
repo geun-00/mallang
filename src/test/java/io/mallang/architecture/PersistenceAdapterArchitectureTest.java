0383394003fb723c1e48fce5471adc06a0296b01
GeunYeong <jgy091411@gmail.com>
2026-04-09 22:26:35 +0900

    :white_check_mark: test : adapter-persistence 아키텍처 테스트 추가

---
+++ src/test/java/io/mallang/architecture/PersistenceAdapterArchitectureTest.java
@@ -0,0 +1,55 @@
+package io.mallang.architecture;
+
+import com.tngtech.archunit.core.domain.JavaClasses;
+import com.tngtech.archunit.core.importer.ImportOption;
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
+import org.springframework.stereotype.Repository;
+
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
+
+@AnalyzeClasses(
+        packages = "io.mallang",
+        importOptions = ImportOption.DoNotIncludeTests.class
+)
+class PersistenceAdapterArchitectureTest {
+
+    @ArchTest
+    void 영속성_어댑터는_application_service_구현체를_직접_의존하면_안_된다(JavaClasses classes) {
+        noClasses()
+                .that().resideInAPackage("..adapter.persistence..")
+                .should().dependOnClassesThat()
+                .resideInAPackage("..application.service..")
+                .check(classes);
+    }
+
+    @ArchTest
+    void 영속성_어댑터_구현체는_required_port를_의존해야_한다(JavaClasses classes) {
+        classes()
+                .that().resideInAPackage("..adapter.persistence.*")
+                .and().haveSimpleNameEndingWith("PersistenceAdapter")
+                .should().dependOnClassesThat()
+                .resideInAPackage("..application.required..")
+                .check(classes);
+    }
+
+    @ArchTest
+    void 영속성_어댑터_구현체_이름은_PersistenceAdapter로_끝나야_한다(JavaClasses classes) {
+        classes()
+                .that().resideInAPackage("..adapter.persistence.*")
+                .and().areAnnotatedWith(Repository.class)
+                .and().areNotInterfaces()
+                .should().haveSimpleNameEndingWith("PersistenceAdapter")
+                .check(classes);
+    }
+
+    @ArchTest
+    void JPA_리포지토리_이름은_Repository로_끝나야_한다(JavaClasses classes) {
+        classes()
+                .that().resideInAPackage("..adapter.persistence.jpa")
+                .and().areInterfaces()
+                .should().haveSimpleNameEndingWith("Repository")
+                .check(classes);
+    }
+}
