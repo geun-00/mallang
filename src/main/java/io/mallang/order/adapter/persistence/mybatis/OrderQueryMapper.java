2d70440638fdb577cbb1235f4d8fa492e241f7ca
GeunYeong <jgy091411@gmail.com>
2026-04-16 12:34:29 +0900

    :sparkles: feat : LoadOrderDetailPort 구현

--- src/main/java/io/mallang/order/adapter/persistence/mybatis/OrderQueryMapper.java
+++ src/main/java/io/mallang/order/adapter/persistence/mybatis/OrderQueryMapper.java
@@ -1,8 +1,10 @@
 package io.mallang.order.adapter.persistence.mybatis;
 
 import io.mallang.order.adapter.persistence.mybatis.model.OrderListRow;
+import io.mallang.order.adapter.persistence.mybatis.model.OrderDetailRow;
 import io.mallang.order.adapter.persistence.mybatis.model.SearchMyOrdersCondition;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
@@ -10,4 +12,6 @@ import java.util.List;
 public interface OrderQueryMapper {
 
     List<OrderListRow> selectMyOrders(SearchMyOrdersCondition condition);
+
+    List<OrderDetailRow> selectOrderDetailRows(@Param("orderIdValue") String orderIdValue);
 }
